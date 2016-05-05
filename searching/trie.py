637db26cd8f43938f5f5225ce9481ef1e1acacde
Akash <akashj1708@gmail.com>
2016-05-05 20:01:07 +0530

    trie

--- searching/trie.py
+++ searching/trie.py
@@ -5,6 +5,8 @@ class TrieNode(object):
 		self.complete_word = False
 		
 	def add_word(word):
+		if is_integer(word):
+			return
 		if not word:
 			self.complete_word = True
 			return
@@ -18,4 +20,18 @@ class TrieNode(object):
 			trie_node.add_word(remaining_word)
 			
 	def search_word(word):
-		pass
+		if len(word) == 0:
+			print "%s found in the records" % word
+			return
+		key = word[0]
+		remaining_word = word[1:]
+		if key in self.next:
+			node = self.next.get(key)
+			if len(remaining_word) == 0:
+				if node.complete_word:
+					print "word found in the records"
+				else:
+					print "word not found in the records"
+				return
+			node.search(remaining_word)
+		print "word not found in the records"
