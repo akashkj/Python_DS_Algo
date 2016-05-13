91ce76a01aee32033bf87b32798fe70922878b33
Akash <akashj1708@gmail.com>
2016-05-13 23:54:46 +0530

    bug fix

--- puzzles/check_palindrome_recursively.py
+++ puzzles/check_palindrome_recursively.py
@@ -5,10 +5,11 @@ class Palindrome(object):
 		if len(input_string) in [0,1]:
 			return True
 		if input_string[0] == input_string[-1]:
-			return Palindrome.is_palindrome(input_string[1:-2])
+			return Palindrome.is_palindrome(input_string[1:-1])
 		return False
-		
-if __name__ == "__main__"
+
+
+if __name__ == "__main__":
 	print Palindrome.is_palindrome("")
 	print Palindrome.is_palindrome("a")
 	print Palindrome.is_palindrome("abcbcbasjfhiatjhuiweufh")
