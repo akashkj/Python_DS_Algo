d75d9c88e797a479d19b52c1a9bce2211659757c
Akash <akashj1708@gmail.com>
2016-05-14 15:49:33 +0530

    added dll, bugfix in ll

--- linked_list/doubly_linked_list.py
+++ linked_list/doubly_linked_list.py
@@ -1,23 +1,25 @@
-class LinkNode(object):
+class DoubleLinkNode(object):
 
 	def __init__(self, val):
 		self.val = val
 		self.next = None
-
-
-class LinkedList(object):
+		self.prev = None
+		
+		
+class DoublyLinkedList(object):
 
 	def __init__(self):
 		self.head = None
 		self.current = None
-		
-	def create_linked_list_from_list(self, input_list):
+
+	def create_linked_list(input_list):
 		for entry in input_list:
-			node = LinkNode(entry)
+			node = DoubleLinkNode(entry)
 			if not self.head:
 				self.head = node
 				self.current = node
 			else:
+				node.prev = self.current
 				self.current.next = node
 				self.current = node
 		return self.head
